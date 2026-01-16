69858705050a012f51615f1277c2d9b11ee68fe1
Ivan Savickiy <isavitskiy12@gmail.com>
2026-01-17 02:04:20 +0300

    codecrafters submit [skip ci]

--- app/main.go
+++ app/main.go
@@ -4,7 +4,37 @@ import (
 	"bufio"
 	"fmt"
 	"os"
+	"strings"
 )
+
+type command struct {
+	name    string
+	execute func() error
+}
+
+var knownCommand = map[string]command{
+	"exit": {
+		name: "exit",
+		execute: func() error {
+			os.Exit(0)
+			return nil
+		},
+	},
+}
+
+func handleCommand(command string) error {
+	if cmd, exists := knownCommand[command]; exists {
+		if err := cmd.execute(); err != nil {
+			fmt.Printf("Error executing command: %s", err)
+			return err
+		}
+
+	} else {
+		fmt.Printf("%s: command not found\n", command)
+	}
+
+	return nil
+}
 
 func main() {
 	scanner := bufio.NewScanner(os.Stdin)
@@ -12,13 +42,14 @@ func main() {
 	fmt.Print("$ ")
 	for scanner.Scan() {
 		command := scanner.Text()
+		command = strings.Trim(command, " ")
 
 		if err := scanner.Err(); err != nil {
-			fmt.Print("fail to read input")
+			fmt.Printf("Error reading command: %s", err)
 			os.Exit(1)
 		}
+		handleCommand(command)
 
-		fmt.Printf("%s: command not found\n", command)
 		fmt.Print("$ ")
 	}
 }
