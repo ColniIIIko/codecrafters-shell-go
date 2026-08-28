0e60e15f1cefaa96ffd4f32259acdc2964540607
Ivan Savitskiy <isavitskiy12@gmail.com>
2026-08-28 14:22:48 +0300

    codecrafters submit [skip ci]

--- app/cmd/echo.go
+++ app/cmd/echo.go
@@ -6,5 +6,9 @@ import (
 
 func Echo(args []string, ctx any) (string, error) {
 	out := strings.Join(args, " ")
+	// by default applying new line at the end if it not exists
+	if out[len(out)-1] != '\n' {
+		out += "\n"
+	}
 	return out, nil
 }
